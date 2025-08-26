6534946d93c27d42f061befff13a466a9a53682b
iozimniy <Kitriss@gmail.com>
2025-08-26 12:39:52 +0300

    убрала старый конфиг для stats без удаления класса конфига

--- core/main-service/src/main/java/ru/practicum/config/StatsClientConfig.java
+++ core/main-service/src/main/java/ru/practicum/config/StatsClientConfig.java
@@ -2,9 +2,7 @@ package ru.practicum.config;
 
 import lombok.Data;
 
-//@ConfigurationProperties("stats.client")
 @Data
-//@Component
 public class StatsClientConfig {
 
     //@Value("${stats.client.host}")
