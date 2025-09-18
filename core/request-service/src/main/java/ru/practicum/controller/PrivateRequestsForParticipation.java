98e7a40e4658dc4157b30647e799327f26b1b2ce
iozimniy <Kitriss@gmail.com>
2025-09-18 21:07:17 +0300

    create interface for ParticipationRequestService

--- core/request-service/src/main/java/ru/practicum/controller/PrivateRequestsForParticipation.java
+++ core/request-service/src/main/java/ru/practicum/controller/PrivateRequestsForParticipation.java
@@ -5,10 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.request.client.ParticipationRequestClient;
-import ru.practicum.request.constants.ParticipationRequestStatus;
 import ru.practicum.request.dto.ParticipationRequestDto;
 import ru.practicum.service.ParticipationRequestService;
+import ru.practicum.service.ParticipationRequestServiceImpl;
 //import ru.practicum.dto.ParticipationRequestDto;
 
 import java.util.List;
--- core/request-service/src/main/java/ru/practicum/service/ParticipationRequestService.java
+++ core/request-service/src/main/java/ru/practicum/service/ParticipationRequestService.java
@@ -1,134 +1,25 @@
 package ru.practicum.service;
 
-import jakarta.persistence.EntityNotFoundException;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.event.client.EventClient;
-import ru.practicum.event.dto.EventFullDto;
-import ru.practicum.mapper.ParticipationRequestToDtoMapper;
-import ru.practicum.model.ParticipationRequest;
-import ru.practicum.repository.ParticipationRequestRepository;
-import ru.practicum.request.constants.ParticipationRequestStatus;
 import ru.practicum.request.dto.ParticipationRequestDto;
 import ru.practicum.request.dto.ParticipationRequestUpdateStatusDto;
-import ru.practicum.user.client.UserClient;
-import ru.practicum.user.dto.UserDto;
-import ru.practicum.validation.ParticipationRequestValidator;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-@Slf4j
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class ParticipationRequestService {
+public interface ParticipationRequestService {
+    List<ParticipationRequestDto> getUserRequests(Long userId);
 
-    private final ParticipationRequestRepository requestRepository;
-    private final EventClient eventClient;
+    ParticipationRequestDto addParticipationRequest(Long userId, Long eventId);
 
-    private final UserClient userClient;
+    ParticipationRequestDto cancelRequest(Long userId, Long requestId);
 
-    private final ParticipationRequestValidator participationRequestValidator;
+    List<ParticipationRequestDto> getUserRequestsByEventId(Long eventId);
 
-    public List<ParticipationRequestDto> getUserRequests(Long userId) {
-        userClient.getUser(userId);
-        return requestRepository.findByUserId(userId)
-                .stream()
-                .map(ParticipationRequestToDtoMapper::mapToDto)
-                .toList();
-    }
+    List<ParticipationRequestDto> getRequestsByIds(List<Long> ids);
 
-    public int getConfirmedRequests(long eventId) {
-        return requestRepository
-                .countConfirmedRequestsByStatusAndEventId(ParticipationRequestStatus.CONFIRMED, eventId);
-    }
+    void updateStatusByIds(ParticipationRequestUpdateStatusDto requestUpdateStatusDto);
 
-    @Transactional
-    public ParticipationRequestDto addParticipationRequest(Long userId, Long eventId) {
-        log.info("Request for add ParticipationRequest with user id {} and event id {}", userId, eventId);
-        UserDto userDto = null;
+    int getConfirmedRequests(Long eventId);
 
-        try {
-            userDto = userClient.getUser(userId);
-        } catch (Exception e) {
-            log.error("Request for get user with id {} to userClient is failed with message {}", userId, e);
-        }
-
-
-        EventFullDto event = null;
-
-        try {
-           event = eventClient.getEventById(eventId);
-           log.debug("Event from EventClient: {}", event);
-        } catch (Exception e) {
-            log.error("Request for get event with id {} to eventClient is failed with message {}", eventId, e);
-        }
-
-        long confirmedRequestsCount = getConfirmedRequests(eventId);
-
-        RuntimeException validationError =
-                participationRequestValidator.checkRequest(userDto, event, confirmedRequestsCount);
-
-        if (validationError != null)
-            throw validationError;
-
-        ParticipationRequest request = ParticipationRequest.builder()
-                .userId(userDto.getId())
-                .eventId(eventId)
-                .build();
-
-        if (event.getParticipantLimit() == 0) {
-            request.setStatus(ParticipationRequestStatus.CONFIRMED);
-        } else {
-            request.setStatus(event.isRequestModeration() ? ParticipationRequestStatus.PENDING : ParticipationRequestStatus.CONFIRMED);
-        }
-
-        request.setCreated(LocalDateTime.now());
-        log.debug("New ParticipationRequest for create: {}", request);
-
-
-        ParticipationRequest savedRequest = requestRepository.save(request);
-        return ParticipationRequestToDtoMapper.mapToDto(savedRequest);
-    }
-
-    @Transactional
-    public ParticipationRequestDto cancelRequest(Long userId, Long requestId) {
-        ParticipationRequest request = requestRepository.findByIdAndUserId(requestId, userId)
-                .orElseThrow(() -> new EntityNotFoundException("Request with id=" + requestId + " was not found"));
-
-        request.setStatus(ParticipationRequestStatus.CANCELED);
-        requestRepository.save(request);
-
-        return ParticipationRequestToDtoMapper.mapToDto(request);
-    }
-
-
-    public List<ParticipationRequestDto> getUserRequestsByEventId(Long eventId) {
-        return requestRepository.findByEventId(eventId).stream()
-                .map(request -> ParticipationRequestToDtoMapper.mapToDto(request))
-                .toList();
-    }
-
-    public List<ParticipationRequestDto> getRequestsByIds(List<Long> ids) {
-        return requestRepository.findByIds(ids).stream()
-                .map(request -> ParticipationRequestToDtoMapper.mapToDto(request))
-                .toList();
-    }
-
-    @Transactional
-    public void updateStatusByIds(ParticipationRequestUpdateStatusDto requestUpdateStatusDto) {
-        requestRepository.updateStatusByIds(requestUpdateStatusDto.getStatus(), requestUpdateStatusDto.getRequestIds());
-    }
-
-    public Map<Long, Integer> getConfirmedRequestsForList(List<Long> ids) {
-        return ids.stream().collect(Collectors.toMap(
-                s -> s,
-                s -> getConfirmedRequests(s)
-        ));
-    }
+    Map<Long, Integer> getConfirmedRequestsForList(List<Long> ids);
 }
